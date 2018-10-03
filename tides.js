c4f84201a86c2f09ab30952d2ed37bcdf0c1428b
Iain Moncrief <Iainmoncrief@gmail.com>
2018-10-03 12:48:02 -1000

    css Tweaks

--- tides.js
+++ tides.js
@@ -40,14 +40,12 @@ Module.register("tides",{
             this.file('Network.js')
         ]
     },
-    // getStyles: function() {
-    //     return [
-    //         'script.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
-    //         'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be avialable in the module folder.
-    //         this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
-    //         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
-    //     ]
-    // },
+    getStyles: function() {
+        return [
+            this.file('main.css'), // this file will be loaded straight from the module folder.
+        ]
+    },
+
     start: function() {
         this.mySpecialProperty = "So much wow!";
         Log.log(this.name + ' is started!');
