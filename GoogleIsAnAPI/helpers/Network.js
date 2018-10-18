fa099337c412567044c52b95a63dce00290b08f6
Iain Moncrief <Iainmoncrief@gmail.com>
2018-10-17 14:54:22 -1000

    created JSON grab and cahing

---
+++ GoogleIsAnAPI/helpers/Network.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+class Network {}
+
+Network.pullJSON = function (url, callback) {
+    fetch(url)
+        .then( function(response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            callback(myJson);
+        });
+}
+
+module.exports = {
+    Network: Network
+};
